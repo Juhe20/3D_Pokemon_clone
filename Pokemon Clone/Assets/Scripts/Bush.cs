5cf975802733f07b222afe50a15ab5f4c1be6109
Jonas Urup Henriksen <Juhe20@student.aau.dk>
2024-11-19 13:03:56 +0100

    Moved scripts around

---
+++ Pokemon Clone/Assets/Scripts/Bush.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bush : MonoBehaviour
+{
+        public List<Transform> spawnLocations;
+}
--- Pokemon Clone/Assets/Scripts/PokemonSpawner.cs
+++ Pokemon Clone/Assets/Scripts/PokemonSpawner.cs
@@ -1,15 +1,45 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PokemonSpawner : MonoBehaviour
 {
-    public GameObject[] pokemonPrefabs;
-    public Transform[] spawnLocations;
-    void SpawnPokemon(string name, int health, int attack, int defense, string type, int speed)
-    {
-        GameObject randomSpawn = pokemonPrefabs[Random.Range(0, pokemonPrefabs.Length)];
+    public GameObject pokemonPrefab;
+    public List<Bush> bushes;
+    public float spawnRange = 5f;
 
+    public Transform player;
+
+    private HashSet<Transform> spawnedLocations = new HashSet<Transform>();
+
+    void Start()
+    {
+        bushes = new List<Bush>(FindObjectsOfType<Bush>());
     }
 
+    void Update()
+    {
+        foreach (var bush in bushes)
+        {
+            foreach (var spawnLocation in bush.spawnLocations)
+            {
+                if (Vector3.Distance(player.position, spawnLocation.position) <= spawnRange)
+                {
+                    if (!spawnedLocations.Contains(spawnLocation))
+                    {
+                        SpawnPokemonNearBush(spawnLocation);
+                        spawnedLocations.Add(spawnLocation); // Prevent multiple spawns
+                    }
+                }
+                else
+                {
+                    spawnedLocations.Remove(spawnLocation); // Allow re-spawning if player leaves range
+                }
+            }
+        }
+    }
+
+    void SpawnPokemonNearBush(Transform spawnLocation)
+    {
+        Instantiate(pokemonPrefab, spawnLocation.position, Quaternion.identity);
+    }
 }
